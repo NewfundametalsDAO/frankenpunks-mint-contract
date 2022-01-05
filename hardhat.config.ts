cfd27cbc19f52619b254ebe062c13615724cc3d6
this-is-obvs <this-is-obvs@users.noreply.github.com>
2022-01-05 15:26:52 -0800

    Config for Rinkeby / misc

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -19,12 +19,14 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
   }
 });
 
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
-
 const config: HardhatUserConfig = {
   solidity: "0.8.9",
   networks: {
+    rinkeby: {
+      url: process.env.RINKEBY_URL || "",
+      accounts:
+        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    },
     ropsten: {
       url: process.env.ROPSTEN_URL || "",
       accounts:
